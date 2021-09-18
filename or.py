ee473096d9175aa8f7dbc1fbf21a596890d5b220
Amit <amit.chawarekar@gmail.com>
2021-09-18 17:39:23 +0530

    docstring updated

--- or.py
+++ or.py
@@ -1,3 +1,9 @@
+    """
+   author : Amit
+   email : amit.chawarekar@gmail.com
+    """
+
+
 from utils.model import Perceptron 
 from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+print("Print")
+__version__ == "0.0.1"
